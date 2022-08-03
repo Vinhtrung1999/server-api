d5e8ef6410310e6b0419837bdb385a96a1fc1681
Trung <trungtran.110199@gmail.com>
2022-08-03 11:28:19 +0700

    1st

---
+++ data.js
@@ -0,0 +1,310 @@
+module.exports = [
+    {
+        id: '01',
+        title: 'Ông Nguyễn Đức Chung được đề nghị giảm án sau khi nộp 85 bằng khen',
+        content: 'VKS đánh giá việc cựu chủ tịch Nguyễn Đức Chung nộp 85 bằng khen, giấy khen, kỷ niệm chương... là tình tiết mới nên đề nghị tòa phúc thẩm giảm một phần hình phạt.',
+        image: 'https://i1-vnexpress.vnecdn.net/2022/07/12/ngNguyncChung-1657615285-2194-1657615424.jpg?w=680&h=408&q=100&dpr=1&fit=crop&s=sV_PXLMMsK7cLjRPNuwP_A',
+        type: 'CT'
+    },
+    {
+        id: '02',
+        title: 'Trung Quốc nêu yêu cầu cải thiện quan hệ, Australia bác bỏ',
+        content: 'Thủ tướng Albanese nói Australia không đáp ứng các yêu cầu sau khi Trung Quốc nêu 4 đề nghị để cải thiện quan hệ song phương.',
+        image: 'https://i1-vnexpress.vnecdn.net/2022/07/12/afp-com-20220710-partners-051-9944-8807-1657615768.jpg?w=220&h=132&q=100&dpr=1&fit=crop&s=NTnbCglCi4Xg5mMkkozoyQ',
+        type: 'CT'
+    },
+    {
+        id: '03',
+        title: 'Ukraine xác nhận tập kích kho vũ khí Nga ở Kherson',
+        content: 'Quân đội Ukraine cho biết đã tấn công mục tiêu quân sự và kho đạn Nga ở Kherson, nhưng không tiết lộ chủng loại vũ khí trong đợt tập kích.',
+        image: 'https://i1-vnexpress.vnecdn.net/2022/07/12/kho-dan-1-1657611805-7363-1657611978.jpg?w=220&h=132&q=100&dpr=1&fit=crop&s=CiVvRTThGBW8x6u6N9Ye0A',
+        type: 'CT'
+    },
+    {
+        id: '05',
+        title: 'TP Thủ Đức có tân Bí thư',
+        content: 'Ông Nguyễn Hữu Hiệp, 55 tuổi, Trưởng ban Dân vận Thành uỷ TP HCM, được chỉ định làm Bí thư Thành uỷ Thủ Đức thay ông Nguyễn Văn Hiếu. ',
+        image: 'https://i1-vnexpress.vnecdn.net/2022/07/12/nguyen-huu-hiep-7778-1657610803.jpg?w=220&h=132&q=100&dpr=1&fit=crop&s=mlsVuEhxr8JaC6GXdKstgg',
+        type: 'CT'
+    },
+    {
+        id: '06',
+        title: 'VFF: \'Tính đối đầu là hợp lý ở U19 Đông Nam Á\'',
+        content: 'Theo quyền Chủ tịch VFF Trần Quốc Tuấn, Indonesia không nên phàn nàn bởi luật chơi công bằng và đã được thông qua ở giải U19 Đông Nam Á 2022.',
+        image: 'https://i1-thethao.vnecdn.net/2022/07/12/img3239-1657596220-4575-165759-6794-3617-1657610442.jpg?w=220&h=132&q=100&dpr=1&fit=crop&s=m-B7PgjJnaGBEcFIlDhxdg',
+        type: 'TT'
+    },
+    {
+        id: '07',
+        title: 'Chuyên gia Indonesia: \'Việt Nam, Thái Lan chẳng làm gì sai\'',
+        content: 'Chuyên gia Akmal Marhali cho rằng chính Indonesia đã đề xuất tiêu chí đối đầu ở U19 Đông Nam Á',
+        image: 'https://i1-thethao.vnecdn.net/2022/07/12/1169737114-1657598010-16575980-5830-9142-1657598057.jpg?w=380&h=228&q=100&dpr=1&fit=crop&s=76si3r2e0JSfFMh7rKnuAw',
+        type: 'TT'
+    },
+    {
+        id: '08',
+        title: 'Pogba: \'Tôi muốn tìm lại niềm vui chơi bóng\'',
+        content: 'Tiền vệ Paul Pogba muốn tìm lại cảm hứng thi đấu tại Turin, và gọi việc trở lại Juventus là "về nhà".',
+        image: 'https://i1-thethao.vnecdn.net/2022/07/12/fxyswl0xwaiqexq-1657601219-165-5969-1117-1657601484.jpg?w=1020&h=0&q=100&dpr=1&fit=crop&s=sRUwf5BG2niEryA3OGNIOg',
+        type: 'TT'
+    },
+    {
+        id: '09',
+        title: 'Federer lần đầu không được xếp hạng trong 25 năm',
+        content: 'Roger Federer tuần này văng khỏi bảng điểm ATP đơn nam lần đầu tiên kể từ năm 1997, sau 12 tháng nghỉ thi đấu.',
+        image: 'https://i1-thethao.vnecdn.net/2022/07/12/1-1657590255-7069-1657590297.jpg?w=220&h=132&q=100&dpr=1&fit=crop&s=IAVs1scQ2AYy3LHSaWYyHQ',
+        type: 'TT'
+    },
+    {
+        id: '10',
+        title: 'Kane, Son đổ gục sau bài tập thể lực của Conte',
+        content: 'Các cầu thủ Tottenham kiệt sức, trong đó Harry Kane thậm chí nôn khan, sau buổi tập dài hai tiếng dưới tiết trời oi bức chiều 11/7.',
+        image: 'https://i1-thethao.vnecdn.net/2022/07/12/nintchdbpict000746525842-16575-3289-6400-1657581836.jpg?w=220&h=132&q=100&dpr=1&fit=crop&s=VuXh3aF2hTt60DYLooO5tQ',
+        type: 'TT'
+    },
+    {
+        id: '11',
+        title: 'The Debate Challenge công bố kiến nghị cho vòng đấu loại',
+        content: 'Ban chuyên môn đưa ra 6 kiến nghị cho bảng Tiếng Việt và Tiếng Anh, giúp thí sinh có thời gian nghiên cứu, tăng khả năng chiến thắng khi đấu loại.',
+        image: 'https://i1-vnexpress.vnecdn.net/2022/07/12/img-1125-1657596283-8866-1657596308.jpg?w=220&h=132&q=100&dpr=1&fit=crop&s=jl69Ezc7dDKQtQS6B2m0pA',
+        type: 'GD'
+    },
+    {
+        id: '12',
+        title: 'Nam sinh trúng tuyển 6 nguyện vọng không đi học thêm',
+        content: 'Phạm Gia Khánh, cựu học sinh THCS Giảng Võ, đỗ tất cả nguyện vọng và trúng tuyển thêm hai trường khác tại Hà Nội mà không đi học thêm.',
+        image: 'https://i1-vnexpress.vnecdn.net/2022/07/12/khanh2-8603-1657591806.jpg?w=1020&h=0&q=100&dpr=1&fit=crop&s=ZszXvEqf5W34DsXTuOJ5-Q',
+        type: 'GD'
+    },
+    {
+        id: '13',
+        title: '7 bí quyết học thạc sĩ tại trường hàng đầu thế giới',
+        content: 'Lấy bằng thạc sĩ từ Đại học Cambridge, Phương Nghi cho rằng việc tự học, làm việc nhóm, sử dụng thư viện điện tử là rất quan trọng.',
+        image: 'https://i1-vnexpress.vnecdn.net/2022/07/09/Phuong-Nghi-1-7065-1657344255.png?w=1020&h=0&q=100&dpr=1&fit=crop&s=Bx7Y-yOTVqOoiGqEmuyStg',
+        type: 'GD'
+    },
+    {
+        id: '14',
+        title: 'Ra quân chương trình \'Trường em thay áo mới\'',
+        content: 'Chương trình do quỹ Hy vọng phối hợp tổ chức, sẽ sơn lại ít nhất 35 ngôi trường đã xuống cấp, tạo diện mạo khang trang trước năm học mới tại huyện Lệ Thủy.',
+        image: 'https://i1-vnexpress.vnecdn.net/2022/07/12/Ra-quan-7233-1657601893.jpg?w=1020&h=0&q=100&dpr=1&fit=crop&s=W_plIflVfi7IH4AcyUS8Kw',
+        type: 'GD'
+    },
+    {
+        id: '15',
+        title: 'Những \'ngôi nhà 5.000 đồng\'',
+        content: '5.000 đồng - số tiền sinh viên Đại học Đồng Tháp đóng góp mỗi tháng giúp bạn cùng trường có hoàn cảnh khó khăn xây nhà mới, kiên cố.',
+        image: 'https://i1-vnexpress.vnecdn.net/2022/06/30/haongoinha5000-1656601112-1611-1656601226.jpg?w=680&h=408&q=100&dpr=1&fit=crop&s=lNw7rS_GmS5FPdlHZu5-4A',
+        type: 'GD'
+    },
+    {
+        id: '16',
+        title: 'Rắn hổ mang chúa cắn chết người đàn ông',
+        content: 'Dùng tay bắt con rắn hổ mang chúa bỏ vào bao, người đàn ông 57 tuổi bị con vật cắn vào tay nhưng không đến bệnh viện điều trị, hai ngày sau tử vong.',
+        image: 'https://i1-vnexpress.vnecdn.net/2022/07/12/ran-1657613953-5791-1657614259.jpg?w=680&h=408&q=100&dpr=1&fit=crop&s=oiEUk15T-pwx0jF2MDZ3uQ',
+        type: 'SK'
+    },
+    {
+        id: '17',
+        title: '5 dấu hiệu bàn tay cảnh báo vấn đề sức khỏe',
+        content: 'Lực nắm yếu, gốc móng tay sưng to, xuất hiện mụn đỏ và mụn nước hoặc sưng cứng khớp có thể báo hiệu những vấn đề sức khỏe tiềm ẩn.',
+        image: 'https://i1-suckhoe.vnecdn.net/2022/07/12/maxresdefault-jpeg-1657610149-3413-1657610271.jpg?w=1020&h=0&q=100&dpr=1&fit=crop&s=CMRB6GwS7AMXCsChWu0gag',
+        type: 'SK'
+    },
+    {
+        id: '18',
+        title: 'Mổ nội soi 3D cứu người đàn ông mắc bệnh hiếm',
+        content: 'Bệnh nhân 43 tuổi, nhập viện trong tình trạng khó thở, đau ngực, suy hô hấp nặng, bác sĩ chẩn đoán mắc bệnh kén khí ở phổi, tỷ lệ 22/100.000 người.',
+        image: 'https://i1-suckhoe.vnecdn.net/2022/07/12/293350964-1106386576617097-414-1150-3826-1657606973.jpg?w=1020&h=0&q=100&dpr=1&fit=crop&s=Ws7eVQ16IZezKOJOphV3bg',
+        type: 'SK'
+    },
+    {
+        id: '19',
+        title: 'Cách ngừa đau họng khi nằm điều hòa',
+        content: 'Nằm điều hòa khiến vợ và con tôi bị đau họng, phải há miệng khi ngủ. Xin bác sĩ tư vấn cách dùng điều hòa đúng? (Hùng, 35 tuổi, Hà Nội).',
+        image: 'https://i1-suckhoe.vnecdn.net/2022/07/08/z3551411279923-b664ec7f33ad239-5682-7862-1657281141.jpg?w=300&h=180&q=100&dpr=1&fit=crop&s=vQhGgTSC9vmlibftKrGIsA',
+        type: 'SK'
+    },
+    {
+        id: '20',
+        title: 'Phát hiện ung thư hiếm từ nứt ngứa núm vú',
+        content: 'Bệnh nhân 66 tuổi, nứt ngứa núm vú một năm, đi khám phát hiện bị paget vú - một loại ung thư hiếm gặp.',
+        image: 'https://i1-suckhoe.vnecdn.net/2022/07/12/womanisclutchingherchest111216-9955-5617-1657594191.jpg?w=220&h=132&q=100&dpr=1&fit=crop&s=nrzildrErPkrOM7tFc-96A',
+        type: 'SK'
+    },
+    {
+        id: '21',
+        title: 'Cá kho măng thơm ngon như nhà hàng',
+        content: 'Thịt cá màu nâu sậm như vân gỗ trắc, măng giòn ngấm vị chua cay và quyện vị đậm đà từ cá, càng ăn càng gây nghiện.',
+        image: 'https://i1-giadinh.vnecdn.net/2022/07/12/Thnhphm1-1657601713-2046-1657601877.jpg?w=680&h=408&q=100&dpr=1&fit=crop&s=W0UUqf8gL24qQa7TufjRuQ',
+        type: 'DS'
+    },
+    {
+        id: '22',
+        title: '6 chỗ trong nhà thường bị quên làm sạch',
+        content: 'Trong khi thường lưu tâm dọn dẹp những nơi dễ nhìn thấy như sàn phòng khách, nhà vệ sinh, các gia chủ lại bỏ qua những nơi rất dễ bám bụi bẩn.',
+        image: 'https://i1-giadinh.vnecdn.net/2022/07/12/Indoor-plant-8341-1657597691.jpg?w=1020&h=0&q=100&dpr=1&fit=crop&s=zlWwIfOojHcTMWZNjmjaCQ',
+        type: 'DS'
+    },
+    {
+        id: '23',
+        title: 'Mối tình với bà mẹ hai con của anh kỹ sư Nhật',
+        content: 'Lần đầu gặp chị Tuyên 9 năm trước, Suzuki Fumiaki đã thích người phụ nữ Việt nhưng dặn lòng không được yêu mẹ đơn thân đang nuôi hai con nhỏ.',
+        image: 'https://i1-giadinh.vnecdn.net/2022/07/11/1-8651-1657473508.jpg?w=1020&h=0&q=100&dpr=1&fit=crop&s=y4oOi2e0NOAR65cu-Kgw0w',
+        type: 'DS'
+    },
+    {
+        id: '24',
+        title: 'Vị trí đặt bể nước theo phong thủy',
+        content: 'Phong thủy xác định vị trí xây bể nước nên xây tại các thiên can (giáp, ất, bính, đinh...), tránh xây trên các cung địa chi (tý, sửu, dần, mão...).',
+        image: 'https://i1-giadinh.vnecdn.net/2022/06/30/bephot1-1656563463-7991-1656563694.jpg?w=300&h=180&q=100&dpr=1&fit=crop&s=qP8cO9IJ74lfx-eqt05hBA',
+        type: 'DS'
+    },
+    {
+        id: '25',
+        title: 'Trang trí nội thất bằng tranh ảnh theo phong thủy',
+        content: 'Người xưa đã dùng tranh, tượng, tháp văn xương… để làm đẹp không gian sống và tạo ra ảnh hưởng tích cực đối với sức khỏe và sự nghiệp. ',
+        image: 'https://i1-giadinh.vnecdn.net/2022/06/18/tranhanh1-1655537747-1664-1655538124.jpg?w=180&h=108&q=100&dpr=1&fit=crop&s=7XFmkKJM3MWMEoHbUdmZ_w',
+        type: 'DS'
+    },
+    {
+        id: '26',
+        title: 'Miền Bắc tiếp tục mưa trong hai ngày tới',
+        content: 'Từ hôm nay đến ngày 15/7, Hà Nội cũng như các tỉnh miền Bắc tiếp tục mưa vào đêm và sáng, sau đó khả năng xuất hiện nắng nóng vào ngày 17-19/7.',
+        image: 'https://i1-vnexpress.vnecdn.net/2022/07/13/290631819105647629530101512448-5790-4374-1657677801.jpg?w=680&h=408&q=100&dpr=1&fit=crop&s=SB1B6ld8_UY_OAjHvHTs6A',
+        type: 'TS'
+    },
+    {
+        id: '27',
+        title: 'Cháy chợ bán quần áo lớn nhất tỉnh Bắc Ninh',
+        content: 'Lửa bốc lên từ quầy bán giày dép, sau đó lan rộng ra chợ Đọ bán quần áo thuộc loại lớn nhất tỉnh Bắc Ninh, rạng sáng 13/7.',
+        image: 'https://i1-vnexpress.vnecdn.net/2022/07/13/8bc301a2ece62eb877f7-7749-1657674092.jpg?w=1020&h=0&q=100&dpr=1&fit=crop&s=Y3BhjNvFwbOYBwAiBK-6lg',
+        type: 'TS'
+    },
+    {
+        id: '28',
+        title: 'Bến Tre lập trung tâm giống, hoa kiểng lớn nhất nước',
+        content: 'Tỉnh Bến Tre lập trung tâm giống, hoa kiểng lớn nhất nước ở huyện Chợ Lách, mỗi năm cung ứng 17-25 triệu cây cho 63 tỉnh thành.',
+        image: 'https://i1-vnexpress.vnecdn.net/2022/07/12/DJI-0116-JPG-5634-1657570496.jpg?w=1020&h=0&q=100&dpr=1&fit=crop&s=nr54EaXSL0l_x6bnHjXulg',
+        type: 'TS'
+    },
+    {
+        id: '29',
+        title: 'Nhiều cựu cán bộ giao đất \'vàng\' ở Nha Trang bị khai trừ Đảng',
+        content: 'Năm nguyên lãnh đạo cấp sở, một phó cục thuế bị khai trừ Đảng do sai phạm trong việc giao 7.300 m2 đất ở trung tâm TP Nha Trang cho doanh nghiệp.',
+        image: 'https://i1-vnexpress.vnecdn.net/2022/07/13/c44541ba272ae474bd3b-165767412-9939-7676-1657674647.jpg?w=1020&h=0&q=100&dpr=1&fit=crop&s=wUXM97AyM7Q5jX6zfML21g',
+        type: 'TS'
+    },
+    {
+        id: '30',
+        title: 'Hà Nội dự kiến xây nhà hát 3.500 chỗ sát Hồ Tây',
+        content: 'Nhà hát có diện tích khoảng 13.000 m2, thiết kế nổi trên mặt hồ Đầm Trị, sẽ là không gian trình diễn nghệ thuật, tổ chức các sự kiện văn hóa chính trị của thủ đô.',
+        image: 'https://i1-vnexpress.vnecdn.net/2022/07/12/nha-ha-t-jpg-5865-1657639267.png?w=1020&h=0&q=100&dpr=1&fit=crop&s=c2Sc3UIeZhYAiJW0eN_y-Q',
+        type: 'TS'
+    },
+    {
+        id: '31',
+        title: 'Hacker rao bán \'dữ liệu trường học của 30 triệu người Việt\'',
+        content: 'Dữ liệu cá nhân kèm thông tin về trường lớp được cho là của 30 triệu người Việt đang bị tin tặc rao bán với giá 3.500 USD.',
+        image: 'https://i1-sohoa.vnecdn.net/2022/07/12/hack1-1657634223-7708-1657634234.jpg?w=220&h=132&q=100&dpr=1&fit=crop&s=zuM6nPNWL_8LD337rM2-ng',
+        type: 'SH'
+    },
+    {
+        id: '32',
+        title: 'Elon Musk đang hủy hoại Twitter thế nào',
+        content: 'Lời đề nghị mua Twitter rồi rút lui của Elon Musk chỉ trong hai tháng khiến giới chuyên gia nhận định thương vụ như trò đùa quá trớn của ông.',
+        image: 'https://variety.com/wp-content/uploads/2022/07/Elon-Musk-Twitter-Lawsuit.jpg?w=681&h=383&crop=1',
+        type: 'SH'
+    },
+    {
+        id: '33',
+        title: 'iPhone 13 bị cấm bán ở Colombia',
+        content: 'Một số sản phẩm mới của Apple như iPhone và iPad phải dừng bán do liên quan tranh chấp bằng sáng chế với Ericsson.',
+        image: 'https://i1-sohoa.vnecdn.net/2022/07/12/thai-nguyen-xmg5dm6l89k-unspla-5161-6175-1657587127.jpg?w=220&h=132&q=100&dpr=1&fit=crop&s=6Z_n-K6yfmwl27qBzI4NKw',
+        type: 'SH'
+    },
+    {
+        id: '34',
+        title: 'Làn sóng startup phát triển AI cho quân sự',
+        content: 'Chiến sự Ukraine thúc đẩy quân đội nhiều nước cải thiện năng lực quân sự, trong đó có đầu tư phát triển công nghệ trí tuệ nhân tạo hiện đại. ',
+        image: 'https://i1-sohoa.vnecdn.net/2022/07/11/gaasigroundcontrolstation-1657-9670-6983-1657511835.jpg?w=220&h=132&q=100&dpr=1&fit=crop&s=yJyXNE39lmqWsK5gsHHlhA',
+        type: 'SH'
+    },
+    {
+        id: '35',
+        title: 'Đi siêu thị online qua kính thông minh',
+        content: 'Tại cửa hàng 7-Eleven ở Iizuna, thị trấn với những vườn táo và nho của Nhật Bản, nhân viên đang đeo cặp kính như trong phim khoa học viễn tưởng. ',
+        image: 'https://i1-sohoa.vnecdn.net/2022/07/11/glasses-01-1657529210-4317-1657529288.jpg?w=220&h=132&q=100&dpr=1&fit=crop&s=jKD3CBvcx9oSezrGbPj8tg',
+        type: 'SH'
+    },
+    {
+        id: '36',
+        title: 'Mưu kế của nam sinh yêu đơn phương',
+        content: 'Nhiều lần bị từ chối tình cảm, Chu Hải Dương nghĩ \'thứ mình không có\' thì người khác cũng đừng hòng đạt được.',
+        image: 'https://i1-vnexpress.vnecdn.net/2022/07/12/6828159455744cc5966778f3eab680-6521-6632-1657621569.jpg?w=220&h=132&q=100&dpr=1&fit=crop&s=QPraciUZoPl3m521ar4JAA',
+        type: 'PL'
+    },
+    {
+        id: '37',
+        title: 'Người tông nữ tiếp viên hàng không \'muốn giữ nhà cho mẹ\'',
+        content: 'Nguyễn Trần Hoàng Phong, người lái Mercedes tông nữ tiếp viên hàng không trọng thương, đồng ý bồi thường 1,5 tỷ đồng cho nạn nhân nhưng không muốn bán nhà. ',
+        image: 'https://i1-vnexpress.vnecdn.net/2022/07/12/tong-tiep-vien-hang-khong-JPG-7938-1657606481.jpg?w=1020&h=0&q=100&dpr=1&fit=crop&s=MJ7PbZQu6tmVzCOjPrcHLg',
+        type: 'PL'
+    },
+    {
+        id: '38',
+        title: 'Lĩnh án vì gọi điện cho người yêu cũ 1.023 lần',
+        content: 'Một người đàn ông 38 tuổi quận Chuncheon, tỉnh Gangwon vừa bị tòa án tuyên án một năm tù hôm 11/7 vì đã "khủng bố" điện thoại người yêu cũ 1.023 lần trong 24 ngày. ',
+        image: 'https://i1-giadinh.vnecdn.net/2022/07/12/Bibat-1657621902-3084-1657621939.jpg?w=220&h=132&q=100&dpr=1&fit=crop&s=bOV-bzcOq1XZVzEKvJ6dzQ',
+        type: 'PL'
+    },
+    {
+        id: '39',
+        title: 'Chủ tiệm vàng ở Cà Mau bị bắt',
+        content: 'Bà Huỳnh Kim Chi, 52 tuổi, chủ tiệm kim hoàn ở huyện Đầm Dơi bị bắt với cáo buộc mua hàng chục lượng vàng của nhóm trộm để bán lại, thu lợi bất chính. ',
+        image: 'https://i1-vnexpress.vnecdn.net/2022/07/12/BACHICAMAU-1657615738-9842-1657615994.jpg?w=220&h=132&q=100&dpr=1&fit=crop&s=Mj3y0tXFuAMjCNtgGMbulA',
+        type: 'PL'
+    },
+    {
+        id: '40',
+        title: 'Người mẫu chết thảm vì thói quen không khóa cửa',
+        content: 'Thi thể người mẫu Canada được phát hiện ở cầu thang thoát hiểm trong một tòa nhà sau chưa đầy hai tuần tới Thượng Hải.',
+        image: 'https://i1-vnexpress.vnecdn.net/2022/07/10/1b4161b4cac449ed821972d70cb4c5-4929-4070-1657450398.png?w=1020&h=0&q=100&dpr=1&fit=crop&s=Gsz1LV5lzs8ztkYKyfoF2g',
+        type: 'PL'
+    },
+    {
+        id: '41',
+        title: 'Mất ba chiếc vé máy bay mới về được Việt Nam',
+        content: 'Mua vé máy bay ba lần rồi bay đi bay lại, Kim Châu mới về được Việt Nam, gần như bật khóc, sau chuyến đi Sri Lanka.',
+        image: 'https://i1-dulich.vnecdn.net/2022/07/09/f768034785d746891fc6-3442-1657332736.jpg?w=1020&h=0&q=100&dpr=1&fit=crop&s=85liiOCAtTA2XCcKhgJxQQ',
+        type: 'DL'
+    },
+    {
+        id: '42',
+        title: 'Top khách sạn có bãi biển đẹp nhất Việt Nam',
+        content: 'Đà Nẵng, Phú Quốc, Quy Nhơn... là những nơi có nhiều khách sạn sở hữu bãi biển đẹp nhất Việt Nam, được du khách quốc tế yêu thích.',
+        image: 'https://i1-dulich.vnecdn.net/2022/07/12/intercontinental-danang-city-3986758374-2x1-1657599743.jpg?w=1200&h=0&q=100&dpr=1&fit=crop&s=GUxkAN5KPaJ06pDoij3oYg',
+        type: 'DL'
+    },
+    {
+        id: '43',
+        title: 'Vietjet tạo dấu ấn trong đà tăng trưởng của hàng không Việt',
+        content: 'Mở lại các đường bay trong nước và quốc tế, khai thác tàu bay thân rộng Airbus A330... là cách Vietjet tích cực góp phần vào sự tăng trưởng của hàng không Việt Nam sau dịch. ',
+        image: 'https://i1-dulich.vnecdn.net/2022/07/12/vj-2-1657616534-8661-1657616733.jpg?w=220&h=132&q=100&dpr=1&fit=crop&s=o-GWnBAiS_0iQSxIIsvMzw',
+        type: 'DL'
+    },
+    {
+        id: '44',
+        title: 'Khám phá hang động trên vịnh Bái Tử Long',
+        content: 'Hệ thống hang động trên vịnh Bái Tử Long mang vẻ đẹp nguyên sơ, hầu hết chưa đưa vào khai thác du lịch nên ít du khách biết đến. ',
+        image: 'https://i1-dulich.vnecdn.net/2022/07/04/images1309299-images1369467-ha-4614-1552-1656926777.jpg?w=220&h=132&q=100&dpr=1&fit=crop&s=x47S5fcm_GjnGkIB2QM7Yw',
+        type: 'DL'
+    },
+    {
+        id: '45',
+        title: 'Cô gái gen Z một mình xuyên Việt gần hai tháng',
+        content: 'Nguyễn Thị Thu Hiền, sinh năm 2000, quyết định nghỉ việc lần thứ hai để một mình đi xe máy xuyên Việt.',
+        image: 'https://i1-dulich.vnecdn.net/2022/07/08/289782418-557127055896159-1577-8682-6027-1657247831.jpg?w=1020&h=0&q=100&dpr=1&fit=crop&s=H-TNqe8dk-Dp39470hW-xg',
+        type: 'DL'
+    }
+]
---
+++ index.js
@@ -0,0 +1,116 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const cors = require('cors')
+var data = require('./data')
+// const xmlparser = require('express-xml-bodyparser')
+
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+// app.use(xmlparser())
+
+app.get('/', (req, res) => {
+    return res.json({ code: 0, message: 'Welcome to api' })
+})
+
+//========*** Day 1 ***=======================================
+app.get('/listNews/:page?', (req, res) => {
+    let page = req.params.page || 1;
+    let start = (page * 10) - 10;
+    let end = page * 10;
+    let data_return = data.filter((val, index) => {
+        if (index >= start && index < end) {
+            return val
+        }
+    })
+    return res.json({ code: 0, data: data_return })
+})
+
+app.get('/news-detail/:id', (req, res) => {
+    let id = req.params.id;
+    if (!id)
+        return res.json({ code: 1, message: 'enter id' })
+
+    let data_return = data.filter(val => val.id === id)
+    return res.json({ code: 0, data: data_return })
+})
+
+//========*** Day 2 ***=======================================
+
+//Get data (use query)
+app.get('/getData', (req, res) => {
+    let page = req.query.page || 0
+    if (page !== 0) {
+        let start = (page * 10) - 10;
+        let end = page * 10;
+        let data_return = data.filter((val, index) => {
+            if (index >= start && index < end) {
+                return val
+            }
+        })
+        return res.json({ code: 0, page: 1, length: data_return.length, data: data_return })
+    }
+    return res.json({ code: 0, length: data.length, data })
+
+})
+
+app.get('/getDataByAction', (req, res) => {
+    let action = req.body
+    switch (action.type) {
+        case "id":
+            data_return = data.filter(val => val.id === action.id)
+            return res.json({ code: 0, data: data_return })
+        case "type":
+            data_return = data.filter(val => val.type === action.typeNews)
+            return res.json({ code: 0, data: data_return })
+        default:
+            return res.json({ code: 1, message: 'Error' })
+    }
+})
+
+//create data
+app.post('/addData', (req, res) => {
+    let news = req.body
+    if (news.id && news.title && news.content && news.image && news.type) {
+        let check = data.find(val => val.id == news.id)
+        if (!check) {
+            data = [...data, news]
+            return res.json({ code: 0, message: 'create success', data: news })
+        }
+    }
+
+    return res.json({ code: 1, message: 'Error' })
+})
+
+//update data
+app.put('/updateData', (req, res) => {
+    let news_update = req.body
+    if (news_update.id && news_update.title && news_update.content && news_update.image && news_update.type) {
+        let check = data.find(val => val.id == news_update.id)
+        if (check) {
+            data.forEach((val, index) => {
+                if (val.id === news_update.id) {
+                    data[index] = news_update
+                }
+            })
+            return res.json({ code: 0, message: 'update success', data: news_update })
+        }
+    }
+    return res.json({ code: 1, message: 'Error' })
+})
+
+//delete data
+app.delete('/deleteData', (req, res) => {
+    let { id } = req.body
+    if (id) {
+        let check = data.find(val => val.id == id)
+        if (check) {
+            data = data.filter(val => val.id !== id)
+            return res.json({ code: 0, message: 'delete success', data: check })
+        }
+    }
+    return res.json({ code: 1, message: 'Error' })
+})
+
+app.listen(port, () => console.log(`http://localhost:${port}`))
